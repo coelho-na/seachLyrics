9a5004ccfcad813ee89bda3123997c97e1f79c13
Nadine Coelho <nadinecoelho9@gmail.com>
2021-05-10 19:42:28 -0300

    finish styles and create variables on js file

---
+++ app.js
@@ -0,0 +1,6 @@
+const form = document.querySelector("#form");
+const searchInput = docuemnt.querySelector("#search");
+const songsContainer = document.querySelector("#songs-container");
+const prevAndNextContainer = document.querySelector("#prev-and-next-container");
+
+const apiURL = `https://api.lyrics.ovh`;
